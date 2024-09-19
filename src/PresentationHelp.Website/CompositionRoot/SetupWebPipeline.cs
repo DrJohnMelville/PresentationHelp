da78bd744124d727b37a91ea140a115a28f0d76c
John Melville <JohnMelville@gmail.com>
2024-09-19 12:34:48 -0400

    Can identify users and meetings in the entry web request.

---
+++ src/PresentationHelp.Website/CompositionRoot/SetupWebPipeline.cs
@@ -0,0 +1,24 @@
+﻿using PresentationHelp.Website.Services;
+
+namespace PresentationHelp.Website.CompositionRoot;
+
+public readonly struct SetupWebPipeline (WebApplication app)
+{
+    public void Configure()
+    {
+        app.UseHttpsRedirection();
+        app.UseMiddleware<UserIdService>();
+
+
+        app.MapGet("/{name}", MeetingConsumer);
+
+    }
+
+    private IResult MeetingConsumer(HttpContext ctx, string name)
+    {
+        return Results.Text($"""
+            <h1>Hello {ctx.Items["UserId"]}</h1>
+            <p>You are in meeting: {name}</p>
+            """, "text/html");
+    }
+}
---
+++ src/PresentationHelp.Website/Services/UserIdService.cs
@@ -0,0 +1,16 @@
+﻿namespace PresentationHelp.Website.Services;
+
+public class UserIdService(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (!context.Request.Cookies.TryGetValue("UserId", out var userId))
+        {
+            userId = Guid.NewGuid().ToString();
+            context.Response.Cookies.Append("UserId", userId, new CookieOptions{MaxAge = TimeSpan.FromDays(14)});
+        }
+
+        context.Items["UserId"] = userId;
+        await next(context);
+    }
+}
